8a256ba2a274bca992b3f17bcb3fcdf0e8a06e24
Akash Kumar <piratedvirus@gmail.com>
2018-09-30 16:28:49 +0530

    Improve code readability as per PEP8

--- simple-particle-swarm-optimization.py
+++ simple-particle-swarm-optimization.py
@@ -1,4 +1,4 @@
-#   Forked from Nathan A. Rooy's repository
+# Forked from Nathan A. Rooy's repository
 
 # IMPORT DEPENDENCIES --------------------------------------------------------------------------------------------------
 
@@ -21,58 +21,58 @@ from maingui import Ui_MainWindow   # Main page of the GUI
 def func1(x):
     total=0
     for i in range(len(x)):
-        total+=x[i]**2
+        total += x[i] ** 2
     return total
 
 # MAIN -----------------------------------------------------------------------------------------------------------------
 
 class Particle:
-    def __init__(self,x0):
-        self.position_i=[]          # particle position
-        self.velocity_i=[]          # particle velocity
-        self.pos_best_i=[]          # best position individual
-        self.err_best_i=-1          # best error individual
-        self.err_i=-1               # error individual
+    def __init__(self, x0):
+        self.position_i = []          # particle position
+        self.velocity_i = []          # particle velocity
+        self.pos_best_i = []          # best position individual
+        self.err_best_i = -1          # best error individual
+        self.err_i = -1               # error individual
 
-        for i in range(0,num_dimensions):
-            self.velocity_i.append(random.uniform(-1,1))
+        for i in range(0, num_dimensions):
+            self.velocity_i.append(random.uniform(-1, 1))
             self.position_i.append(x0[i])
 
     # evaluate current fitness
-    def evaluate(self,costFunc):
-        self.err_i=costFunc(self.position_i)
+    def evaluate(self, costFunc):
+        self.err_i = costFunc(self.position_i)
 
         # check to see if the current position is an individual best
-        if self.err_i<self.err_best_i or self.err_best_i==-1:
-            self.pos_best_i=self.position_i
-            self.err_best_i=self.err_i
+        if self.err_i < self.err_best_i or self.err_best_i == -1:
+            self.pos_best_i = self.position_i
+            self.err_best_i = self.err_i
                     
     # update new particle velocity
-    def update_velocity(self,pos_best_g):
-        w=0.5       # constant inertia weight (how much to weigh the previous velocity)
-        c1=1        # cognative constant
-        c2=2        # social constant
+    def update_velocity(self, pos_best_g):
+        w = 0.5       # constant inertia weight (how much to weigh the previous velocity)
+        c1 = 1        # cognative constant
+        c2 = 2        # social constant
         
-        for i in range(0,num_dimensions):
-            r1=random.random()
-            r2=random.random()
+        for i in range(0, num_dimensions):
+            r1 = random.random()
+            r2 = random.random()
             
-            vel_cognitive=c1*r1*(self.pos_best_i[i]-self.position_i[i])
-            vel_social=c2*r2*(pos_best_g[i]-self.position_i[i])
-            self.velocity_i[i]=w*self.velocity_i[i]+vel_cognitive+vel_social
+            vel_cognitive = c1 * r1 * (self.pos_best_i[i] - self.position_i[i])
+            vel_social = c2 * r2 * (pos_best_g[i] - self.position_i[i])
+            self.velocity_i[i] = w * self.velocity_i[i] + vel_cognitive + vel_social
 
     # update the particle position based off new velocity updates
     def update_position(self,bounds):
-        for i in range(0,num_dimensions):
-            self.position_i[i]=self.position_i[i]+self.velocity_i[i]
+        for i in range(0, num_dimensions):
+            self.position_i[i] = self.position_i[i] + self.velocity_i[i]
             
             # adjust maximum position if necessary
-            if self.position_i[i]>bounds[i][1]:
-                self.position_i[i]=bounds[i][1]
+            if self.position_i[i] > bounds[i][1]:
+                self.position_i[i] = bounds[i][1]
 
             # adjust minimum position if neseccary
-            if self.position_i[i]<bounds[i][0]:
-                self.position_i[i]=bounds[i][0]
+            if self.position_i[i] < bounds[i][0]:
+                self.position_i[i] = bounds[i][0]
 
 globalBestPos = []
 globalBestErr = 0
@@ -80,38 +80,38 @@ bestCostsX = []
 bestCostsY = []
 
 class PSO():
-    def __init__(self,costFunc,x0,bounds,num_particles,maxiter):
+    def __init__(self, costFunc, x0, bounds, num_particles, maxiter):
         global num_dimensions
         global globalBestPos
         global globalBestErr
 
-        num_dimensions=len(x0)
-        err_best_g=-1                   # best error for group
-        pos_best_g=[]                   # best position for group
+        num_dimensions = len(x0)
+        err_best_g = -1                   # best error for group
+        pos_best_g = []                   # best position for group
 
         # establish the swarm
-        swarm=[]
-        for i in range(0,num_particles):
+        swarm = []
+        for i in range(0, num_particles):
             swarm.append(Particle(x0))
 
         # begin optimization loop
-        i=0
+        i = 0
         while i<maxiter:
             #print i,err_best_g
             # cycle through particles in swarm and evaluate fitness
-            for j in range(0,num_particles):
+            for j in range(0, num_particles):
                 swarm[j].evaluate(costFunc)
 
                 # determine if current particle is the best (globally)
-                if swarm[j].err_i<err_best_g or err_best_g==-1:
-                    pos_best_g=list(swarm[j].position_i)
-                    err_best_g=float(swarm[j].err_i)
+                if swarm[j].err_i < err_best_g or err_best_g == -1:
+                    pos_best_g = list(swarm[j].position_i)
+                    err_best_g = float(swarm[j].err_i)
 
             # cycle through swarm and update velocities and position
-            for j in range(0,num_particles):
+            for j in range(0, num_particles):
                 swarm[j].update_velocity(pos_best_g)
                 swarm[j].update_position(bounds)
-            i+=1
+            i += 1
 
             bestCostsX.append(pos_best_g[0])
             bestCostsY.append(pos_best_g[1])
@@ -119,9 +119,11 @@ class PSO():
         globalBestErr = err_best_g
         globalBestPos = pos_best_g
 
-        # print final results
-        print ('FINAL:')
+        # print final results to console
+        print ("FINAL:")
+        print ("Global Best Position:")
         print (pos_best_g)
+        print ("Global Best Error:")
         print (err_best_g)
 
         # plot final result
@@ -193,7 +195,9 @@ class AppWindow(QMainWindow):
         maxiter = int(maxiter)
 
         PSO(func1, initial, bounds, num_particles, maxiter)
-        QMessageBox.question(self, 'Result', "Global Best Position: " + str(globalBestPos)+ "\nGlobal Best Error: " + str(globalBestErr), QMessageBox.Ok, QMessageBox.Ok)
+        QMessageBox.question(self, 'Result', "Global Best Position: " + str(globalBestPos)
+                             + "\nGlobal Best Error: " + str(globalBestErr),
+                             QMessageBox.Ok, QMessageBox.Ok)
 
     @pyqtSlot()
     def preset_inputs(self):
@@ -204,11 +208,15 @@ class AppWindow(QMainWindow):
 
     @pyqtSlot()
     def show_members(self):
-        QMessageBox.question(self, 'Members', "Made by:\nAkash Kumar (3312016009001091)\nArijit Roy (331201600900xxxx)", QMessageBox.Ok, QMessageBox.Ok)
+        QMessageBox.question(self, 'Members', "Made by:\nAkash Kumar (3312016009001091)"
+                                              "\nArijit Roy (331201600900xxxx)",
+                             QMessageBox.Ok, QMessageBox.Ok)
 
     @pyqtSlot()
     def help(self):
-        QMessageBox.question(self, 'Help', "Use comma separated values for inputs.\nUse Preset Inputs option from the Menu to use predefined values\n", QMessageBox.Ok, QMessageBox.Ok)
+        QMessageBox.question(self, 'Help', "Use comma separated values for inputs."
+                                           "\nUse Preset Inputs option from the Menu to use predefined values\n",
+                             QMessageBox.Ok, QMessageBox.Ok)
 
 # RUN ------------------------------------------------------------------------------------------------------------------
 
